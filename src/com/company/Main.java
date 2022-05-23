e419671053307434268c0736ebbec2036f935819
Kate <penovakaterina@gmail.com>
2022-05-23 17:25:36 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 6;
+        int c = 2;
+        double y;
+        y = (Math.abs(a-b)/(Math.pow((a+b),3))-Math.sqrt(c));
+        System.out.println(y);
+    }
+
+}
